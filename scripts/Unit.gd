df760d59183e13deb00969ec0d7f2d36243faf32
Rayzeal24 <yukarinegima1@gmail.com>
2026-03-05 13:06:55 +0100

    Create Unit.gd

---
+++ scripts/Unit.gd
@@ -0,0 +1,14 @@
+extends RefCounted
+class_name Unit
+
+var owner: Country
+var unit_type: String = "infantry"
+
+var hp: int = 10
+var atk: int = 3
+var defense: int = 2
+
+var move_points_max: int = 2
+var move_points: int = 2
+
+var region: Region
